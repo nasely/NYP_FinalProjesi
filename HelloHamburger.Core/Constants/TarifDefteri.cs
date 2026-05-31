ffb60e044e292a6a2f322a64a83f23072b081445
Nas <naselifbal@gmail.com>
2026-06-01 00:41:57 +0300

    HelloHamburger.Core

---
+++ HelloHamburger.Core/Constants/TarifDefteri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloHamburger.Core.Constants
+{
+    internal class TarifDefteri
+    {
+    }
+}
