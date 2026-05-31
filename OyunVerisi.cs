396facb8f2bc27dd6334cebe3a00e6098d917c01
Nas <naselifbal@gmail.com>
2026-06-01 00:24:06 +0300

    HelloHamburger.Entities.Concrete

---
+++ OyunVerisi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloHamburger.Entities.Concrete
+{
+    public class OyunVerisi
+    {
+        public int Coin { get; set; }
+        public int TakipciSayisi { get; set; }
+        public int KalinanGun { get; set; }
+
+        public OyunVerisi()
+        {
+            Coin = 0;
+            TakipciSayisi = 25;
+            KalinanGun = 1;
+        }   
+
+    }
+}
